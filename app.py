7f8f4eb4c43767173fac38b381a390a245dd81af
Nemanja Radojkovic <nemanjaradojkovic@nemanja-roots.local>
2019-09-10 00:23:14 +0200

    daily commit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 
 #%%
-import flask
+from flask import Flask
+from flask import render_template
 import os
 
 #%%
 
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
 
 #%%
 
+if __name__ == "__main__":
+    app.run()
