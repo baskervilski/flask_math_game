f0fe13155c07034e13f6cdf9d9231fdf84e57ae5
Nemanja Radojkovic <nemanjaradojkovic@nemanja-roots.local>
2019-09-11 19:52:43 +0200

    routine commit

--- static/jsquiz.js
+++ static/jsquiz.js
@@ -1,25 +1,26 @@
 (function() {
     var questions = [{
-      question: "What is 2*5?",
-      choices: [2, 5, 10, 15, 20],
-      correctAnswer: 2
-    }, {
-      question: "What is 3*6?",
-      choices: [3, 6, 9, 12, 18],
-      correctAnswer: 4
-    }, {
-      question: "What is 8*9?",
-      choices: [72, 99, 108, 134, 156],
-      correctAnswer: 0
-    }, {
-      question: "What is 1*7?",
-      choices: [4, 5, 6, 7, 8],
-      correctAnswer: 3
-    }, {
-      question: "What is 8*8?",
-      choices: [20, 30, 40, 50, 64],
-      correctAnswer: 4
-    }];
+        question: "What is 2*5?",
+        choices: [2, 5, 10, 15, 20],
+        correctAnswer: 2
+      }, {
+        question: "What is 3*6?",
+        choices: [3, 6, 9, 12, 18],
+        correctAnswer: 4
+      }, {
+        question: "What is 8*9?",
+        choices: [72, 99, 108, 134, 156],
+        correctAnswer: 0
+      }, {
+        question: "What is 1*7?",
+        choices: [4, 5, 6, 7, 8],
+        correctAnswer: 3
+      }, {
+        question: "What is 8*8?",
+        choices: [20, 30, 40, 50, 64],
+        correctAnswer: 4
+      }
+    ];
     
     var questionCounter = 0; //Tracks question number
     var selections = []; //Array containing user choices
@@ -86,11 +87,27 @@
       var qElement = $('<div>', {
         id: 'question'
       });
+
+      var operation = ()=>{
+        if (Math.random() >= 0.5) {
+          'add'
+        }
+        else {
+          'subtract'
+        }
+      }();
+
+      var a = Math.round(100*Math.random())
+      var b = Math.round(100*Math.random())
+
+      var q = questions[index].question
+
+      q = 'TEST'
       
-      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      var header = $('<h2>Pitanje ' + (index + 1) + ':</h2>');
       qElement.append(header);
       
-      var question = $('<p>').append(questions[index].question);
+      var question = $('<p>').append(q);
       qElement.append(question);
       
       var radioButtons = createRadios(index);
